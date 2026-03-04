4a067f8b21c4f49e875783e8f3c595d38b85f9ac
Rintaro Kikuchi <KIKUCHI-Rintaro@tdi.co.jp>
2026-03-05 01:03:31 +0900

    API

--- app/python/main.py
+++ app/python/main.py
@@ -1,3 +1,24 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+@app.get("/helloworld")
+def get_hello_message():
+    return {"message": "Hello World!"}
+
+@app.get('/api/{message}')
+def get_any_message(message: str):
+    return {"message": message}
+
+class SchemaOfTitanicFeaturesRequest(BaseModel):
+    Sex:str
+    Pclass:str
+    Age:int
+    Parch:int
+    SibSp:int
+    
+
+@app.post('/api/titanic', response_model=SchemaOfTitanicFeaturesRequest)
+def get_titanic_features(request_body: SchemaOfTitanicFeaturesRequest):
+    return request_body
