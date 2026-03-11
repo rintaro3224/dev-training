3678e07287d77dd951d7691081464b72e599cf61
Rintaro Kikuchi <KIKUCHI-Rintaro@tdi.co.jp>
2026-03-11 11:28:59 +0900

    実装完了

--- app/python/main.py
+++ app/python/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from machine_learning import predictOnAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['http://localhost:3000'],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/helloworld")
 def get_hello_message():
